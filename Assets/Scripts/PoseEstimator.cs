2e3edb9bfd3c1822659bf8f929aeed77382860e6
Zheng Diao <zdiao@andrew.ad.cmu.edu>
2017-10-04 19:24:54 -0400

    skeleton fix?

--- Assets/Scripts/PoseEstimator.cs
+++ Assets/Scripts/PoseEstimator.cs
@@ -41,14 +41,19 @@ public class PoseEstimator : Singleton<PoseEstimator> {
 		var body = data[0];
 		if (body==null) return;
 		foreach(JointType joint in System.Enum.GetValues(typeof(JointType))){
-			
 			if(joint == JointType.SpineBase) continue;
-			skeleton[joint].localPosition = skeleton[joint].localPosition.magnitude * ToVector3(body.Joints[joint]); 
+			skeleton[joint].localPosition = skeleton[joint].localPosition.magnitude * GetDirection(body, joint); 
 
 		}
 	}
 
+	Vector3 GetDirection(Body b, JointType j){
+		var parent = KinectSkeleton.GetParent(j).Value();
+		return (ToVector3(b.Joints[j]) - ToVector3(b.Joints[parent])).normalized;
+	}
+
+
 	Vector3 ToVector3(Windows.Kinect.Joint j){
-		return new Vector3(j.Position.X, j.Position.Y, j.Position.Z).normalized;
+		return new Vector3(j.Position.X, j.Position.Y, j.Position.Z);
 	}
 }
