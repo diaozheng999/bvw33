d4a4770e6ce42a1cb06cabaa68ff68dbbd52370f
Zheng Diao <zdiao@andrew.ad.cmu.edu>
2017-10-05 13:03:21 -0400

    updated weights

--- logistic_regression.py
+++ logistic_regression.py
@@ -7,6 +7,8 @@ data1 = pd.read_csv("log3.csv", header=None)
 data2 = pd.read_csv("log2.csv", header=None)
 data3 = pd.read_csv("log1.csv", header=None)
 
+data4 = pd.read_csv("log_more.csv", header=None)
+
 x1 = data1[range(100)]
 y1 = data1[100]
 x2 = data2[range(100)]
@@ -14,10 +16,15 @@ y2 = data2[100]
 x3 = data3[range(100)]
 y3 = data3[100]
 
+x4 = data3[range(100)]
+y4 = data3[100]
+
+
 model = LogisticRegression()
 
 
 
+model.fit(x4, y4)
 model.fit(x3, y3)   
 model.fit(x2, y2)
 
